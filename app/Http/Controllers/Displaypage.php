c12cb58a4b98f6e1b1ac861f5d4e473da5d03fe6
Goodnews <mynext40days@gmail.com>
2023-05-18 11:31:17 +0000

    first commit

---
+++ app/Http/Controllers/Displaypage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Displaypage extends Controller
+{
+    public function Home(){
+        return view('welcome'); 
+        }
+    
+    //
+}
+
+
